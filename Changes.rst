eb7c53629d5d1b3e4395fcbe5f863371a319f6dc
Andrey Kislyuk <kislyuk@gmail.com>
2016-04-12 16:57:16 -0700

    v0.3.2

--- Changes.rst
+++ Changes.rst
@@ -1,3 +1,7 @@
+Version 0.3.2 (2016-04-12)
+--------------------------
+- Treat *_CONFIG_FILE as colon delimited
+
 Version 0.3.1 (2016-04-12)
 --------------------------
 - Fix bug in saving to a relative path
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='tweak',
-    version='0.3.1',
+    version='0.3.2',
     url='https://github.com/kislyuk/tweak',
     license='Apache Software License',
     author='Andrey Kislyuk',
