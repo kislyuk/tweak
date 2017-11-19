kislyuk/tweak

commit 9e6a025ce15dd69d1d40793664683f3db63855b7

Update __init__.py

tweak/__init__.py
@@ -102,7 +102,7 @@ class Config(collections.MutableMapping):
                     loader.flatten_mapping(node)
                     return self._as_config(yaml.Loader.construct_mapping(loader, node))
             ConfigLoader.add_constructor(yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG, ConfigLoader.construct_mapping)
-            return yaml.load(stream, ConfigLoader) or {}
+            return yaml.safe_load(stream, ConfigLoader) or {}
         else:
             return json.load(stream, object_hook=self._as_config)
 

commit bba3ffb1d76da54eccd24eb9b71ba08bdd418273

Fix logic error introduced in 9e6a025, add test

tweak/__init__.py
@@ -101,12 +101,12 @@ class Config(collections.MutableMapping):
         if self._use_yaml:
             import yaml
 
-            class ConfigLoader(yaml.Loader):
+            class ConfigLoader(yaml.SafeLoader):
                 def construct_mapping(loader, node):
                     loader.flatten_mapping(node)
-                    return self._as_config(yaml.Loader.construct_mapping(loader, node))
+                    return self._as_config(yaml.SafeLoader.construct_mapping(loader, node))
             ConfigLoader.add_constructor(yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG, ConfigLoader.construct_mapping)
-            return yaml.safe_load(stream, ConfigLoader) or {}
+            return yaml.load(stream, Loader=ConfigLoader) or {}
         else:
             return json.load(stream, object_hook=self._as_config)
 

test/test.py
@@ -5,7 +5,7 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 
 import os, sys, unittest, tempfile, json, logging, pickle
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))  # noqa
 from tweak import Config
 
 class TestTweak(unittest.TestCase):
@@ -93,6 +93,10 @@ class TestTweak(unittest.TestCase):
             self.assertEqual(dict(config), {'y': 'bar', 'x': list(range(8)), 'z': {'null': None}, 't': 4.5})
             self.assertEqual(len(config.config_files), 4)
 
+            config = Config("TWEAK_TEST", use_yaml=True)
+            self.assertEqual(dict(config), {'y': 'bar', 'x': list(range(8)), 'z': {'null': None}, 't': 4.5})
+            self.assertEqual(len(config.config_files), 4)
+
     def test_ingest_with_merge_operator_failure(self):
         with tempfile.NamedTemporaryFile("w") as cf1, tempfile.NamedTemporaryFile("w") as cf2:
             json.dump(dict(x="foo", y="bar", z={}), cf1)
@@ -118,6 +122,7 @@ class TestTweak(unittest.TestCase):
             config = Config("TWEAK_TEST")
             self.assertEqual(dict(config), {'x': list(range(8)), 'z': {'null': None}, 't': 4.5, 'include': incl_expr})
             self.assertEqual(len(config.config_files), 3)
+
 
 if __name__ == '__main__':
     unittest.main()