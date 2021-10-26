e86b3b6fabd6fc92ed2c43e2e10f4a7321f19421
Andrey Kislyuk <kislyuk@gmail.com>
2021-10-26 16:27:20 -0700

    v1.0.4

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="tweak",
-    version="1.0.3",
+    version="1.0.4",
     url="https://github.com/kislyuk/tweak",
     license="Apache Software License",
     author="Andrey Kislyuk",
